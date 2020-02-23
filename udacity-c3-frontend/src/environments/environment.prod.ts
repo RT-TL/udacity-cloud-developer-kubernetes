0257677a0c6c82888702adeb83d67f9e181f54d9
FIRST_NAME LAST_NAME <Toxa@Toxa.Toxa>
2020-02-23 13:33:32 +0200

    added travis configuration

--- udacity-c3-frontend/src/environments/environment.prod.ts
+++ udacity-c3-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a69ae5384562711ea8e8712474d01f47-76433561.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
